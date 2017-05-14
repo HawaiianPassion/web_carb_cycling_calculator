27381200475f48cdae9aafeae24c1a81378b0499
HawaiianPassion <rysongit@gmail.com>
2017-05-13 20:20:37 -0700

    got nested components to work and pass parameters from a child component to a parent component

--- app.js
+++ app.js
@@ -7,13 +7,32 @@ var APP = angular.module("my_APP", [
  * combo box selections and then pass data to semantic presenational component to interpret.
  */ 
 APP.component('statsForm',{
-    controller: function($scope){
-        $scope.height_field = "22";
-        $scope.weight_field = "45";
-        $scope.age_field = "90";
-        $scope.gender_field = null;
+    controller: function(){
+        this.height_field = "22";
+        this.weight_field = "45";
+        this.age_field = "90";
+        this.gender_field = null;
+    },
+    bindings:{
+        calculate: '&'
+    },
+    template:
+        '<form>' +
+            '<input type="text" name="height" placeholder="enter height" ng-model="$ctrl.height_field"></input>'+
+            '<input type="text" name="weight" placeholder="enter weight" ng-model="$ctrl.weight_field"></input>'+
+            '<input type="text" name="age" placeholder="enter age" ng-model="$ctrl.age_field"></input>'+
+            '<select name="gender" ng-model="$ctrl.gender_field">'+
+                '<option value="">-- Pick your gender --</option>'+
+                '<option value="male">male</option>'+
+                '<option value="female">female</option>'+
+            '</select>'+
+            '<input type="button" value="submit" ng-click="$ctrl.calculate({height: $ctrl.height_field, weight: $ctrl.weight_field, age: $ctrl.age_field, gender: $ctrl.gender_field})"></input>'+        
+        '</form>'
+});
 
-        $scope.calculateBMR= function(height, weight, age, gender){
+APP.component('bmrCalculator',{
+    controller: function(){
+       this.calculateBMR = function(height, weight, age, gender){
             console.log("height: "+height);
             console.log("weight: "+weight);
             console.log("age: "+age);
@@ -21,32 +40,8 @@ APP.component('statsForm',{
         };
     },
     template:
-        '<form>' +
-            '<input type="text" name="height" placeholder="enter height" ng-model="$ctrl.height_field"></input>'+
-            '<input type="text" name="weight" placeholder="enter weight" ng-model="$ctrl.weight_field"></input>'+
-            '<input type="text" name="age" placeholder="enter age" ng-model="$ctrl.age_field"></input>'+
-            '<select name="gender" ng-model="gender_field">'+
-                '<option value="">-- Pick your gender --</option>'+
-                '<option value="male">male</option>'+
-                '<option value="female">female</option>'+
-            '</select>'+
-            '<input type="button" value="submit" onclick="$ctrl.calculateBMR($ctrl.height_field, $ctrl.weight_field, $ctrl.age_field, $ctrl.gender_field)"></input>'+        
-            '</form>'
+        '<stats-form calculate="$ctrl.calculateBMR(height, weight, age, gender)"></stats-form>'
 });
-
-// App.component('bmrCalculator',{
-//     controller: function(){
-//         this.calculateBMR= function(height, weight, age, gender){
-//             console.log("height: "+height);
-//             console.log("weight: "+weight);
-//             console.log("age: "+age);
-//             console.log("gender: "+gender);
-//         };
-//     },
-//     template:
-//             // '<input type="text" name="height" placeholder="enter height" ng-model="$ctrl.height_field"></input>'
-//         '<stats-form></stats-form>'
-// });
 
 /**
  * TODO: atomic presentational component that displays the calculated numbers
