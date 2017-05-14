a43a6f39aa1db92c3b743ff965b182a92ddd8334
HawaiianPassion <rysongit@gmail.com>
2017-05-13 18:37:16 -0700

    first attempt at component

--- app.js
+++ app.js
@@ -6,17 +6,47 @@ var APP = angular.module("my_APP", [
  * TODO: atomic presentational components that will accept user input as text and 
  * combo box selections and then pass data to semantic presenational component to interpret.
  */ 
-APP.component('userDataForm',{
-    controller: function(){
-        this.text = "i got text from a component"
-    },
-    bindings:{
-        submit: '&'
+APP.component('statsForm',{
+    controller: function($scope){
+        $scope.height_field = "22";
+        $scope.weight_field = "45";
+        $scope.age_field = "90";
+        $scope.gender_field = null;
+
+        $scope.calculateBMR= function(height, weight, age, gender){
+            console.log("height: "+height);
+            console.log("weight: "+weight);
+            console.log("age: "+age);
+            console.log("gender: "+gender);
+        };
     },
     template:
-        '<h1><span class="glyphicon glyphicon-tree-conifer"></span>{{$ctrl.text}}</h1>'
-
+        '<form>' +
+            '<input type="text" name="height" placeholder="enter height" ng-model="$ctrl.height_field"></input>'+
+            '<input type="text" name="weight" placeholder="enter weight" ng-model="$ctrl.weight_field"></input>'+
+            '<input type="text" name="age" placeholder="enter age" ng-model="$ctrl.age_field"></input>'+
+            '<select name="gender" ng-model="gender_field">'+
+                '<option value="">-- Pick your gender --</option>'+
+                '<option value="male">male</option>'+
+                '<option value="female">female</option>'+
+            '</select>'+
+            '<input type="button" value="submit" onclick="$ctrl.calculateBMR($ctrl.height_field, $ctrl.weight_field, $ctrl.age_field, $ctrl.gender_field)"></input>'+        
+            '</form>'
 });
+
+// App.component('bmrCalculator',{
+//     controller: function(){
+//         this.calculateBMR= function(height, weight, age, gender){
+//             console.log("height: "+height);
+//             console.log("weight: "+weight);
+//             console.log("age: "+age);
+//             console.log("gender: "+gender);
+//         };
+//     },
+//     template:
+//             // '<input type="text" name="height" placeholder="enter height" ng-model="$ctrl.height_field"></input>'
+//         '<stats-form></stats-form>'
+// });
 
 /**
  * TODO: atomic presentational component that displays the calculated numbers
